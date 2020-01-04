86ac1ea22f1ae561665aa9a7b9e2382f78665ab7
Ryan Foster <phasecorex@gmail.com>
2020-01-04 18:04:19 -0500

    Update to skinview3d v1.2.1

--- themes/material/assets/initializeskinview3d.js
+++ themes/material/assets/initializeskinview3d.js
@@ -6,7 +6,5 @@ let skinViewer = new skinview3d.SkinViewer({
     skinUrl: "https://crafatar.com/skins/" + uuid + ".png"
 });
 let control = new skinview3d.createOrbitControls(skinViewer);
-skinViewer.animation = new skinview3d.CompositeAnimation();
-let walk = skinViewer.animation.add(skinview3d.WalkingAnimation);
-walk.speed = 1.5;
-let rotate = skinViewer.animation.add(skinview3d.RotatingAnimation);
+let walk = skinViewer.animations.add(skinview3d.WalkingAnimation);
+let rotate = skinViewer.animations.add(skinview3d.RotatingAnimation);
